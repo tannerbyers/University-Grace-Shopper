c8fbd58ea7fdb1698eca7fb3b65908743e3ccff8
tannerbyers <programtanner@gmail.com>
2020-03-20 00:22:03 -0400

    added styling for number in OrdersWidget

--- src/components/Orders/OrdersWidget.js
+++ src/components/Orders/OrdersWidget.js
@@ -2,11 +2,14 @@ import React, { useState } from "react";
 
 const OrdersWidget = () => {
   return (
-    <div>
+    <div style={{position: "relative"}}>
       <img
         style={{ width: "30px", height: "30px" }}
         src="https://image.flaticon.com/icons/svg/1374/1374128.svg"
       />
+      <div style={{position: "absolute", top: "-15px", right: "-10px", fontSize: "1rem"}}>
+          100
+      </div>
     </div>
   );
 };
