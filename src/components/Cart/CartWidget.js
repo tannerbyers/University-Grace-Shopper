fb8f07ac917502a41b06a88bd7b4382ae80dde49
Leighton A Pitman <leightonpitman14@gmail.com>
2020-03-29 21:44:25 -0400

    add styling

--- src/components/Cart/CartWidget.js
+++ src/components/Cart/CartWidget.js
@@ -1,15 +1,35 @@
 import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Badge from "@material-ui/core/Badge";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 
-const CartWidget = ({lineItems}) => {
+const CartWidget = ({ lineItems }) => {
+  const useStyles = makeStyles(theme => ({
+    badge: {
+      "& > *": {
+        margin: theme.spacing(1)
+      }
+    }
+  }));
+
+  const classes = useStyles();
+
   return (
-    <div style={{ position: "relative" }}>
-      <img
-        style={{ width: "30px", height: "30px" }}
-        src="https://image.flaticon.com/icons/svg/1374/1374128.svg"
-      ></img>
-      <p style={{ position: "absolute", top: "-35", left: "30" }}> {lineItems.length} </p>
+    <div className={classes.badge}>
+      <Badge badgeContent={lineItems.length} color="error">
+        <ShoppingCartIcon />
+      </Badge>
     </div>
   );
+  // return (
+  //   <div style={{ position: "relative" }}>
+  //     <img
+  //       style={{ width: "30px", height: "30px" }}
+  //       src="https://image.flaticon.com/icons/svg/1374/1374128.svg"
+  //     ></img>
+  //     <p style={{ position: "absolute", top: "-35", left: "30" }}> {lineItems.length} </p>
+  //   </div>
+  // );
 };
 
 export default CartWidget;
