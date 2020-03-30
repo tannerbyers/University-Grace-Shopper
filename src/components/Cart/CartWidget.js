c34f4d307a9d191261bf3da32be1ac1ce247cfbb
tannerbyers <programtanner@gmail.com>
2020-03-29 21:16:10 -0400

    corrected cart widget counter

--- src/components/Cart/CartWidget.js
+++ src/components/Cart/CartWidget.js
@@ -1,13 +1,21 @@
 import React, { useState } from "react";
 
-const CartWidget = ({lineItems}) => {
+const CartWidget = ({ lineItems }) => {
+  let totalItemsInCart = 0;
+  for (let i = 0; i < lineItems.length; i++) {
+    totalItemsInCart += lineItems[i].quantity;
+  }
+
   return (
     <div style={{ position: "relative" }}>
       <img
         style={{ width: "30px", height: "30px" }}
         src="https://image.flaticon.com/icons/svg/1374/1374128.svg"
       ></img>
-      <p style={{ position: "absolute", top: "-35", left: "30" }}> {lineItems.length} </p>
+      <p style={{ position: "absolute", top: "-35", left: "30" }}>
+        {" "}
+        {totalItemsInCart}{" "}
+      </p>
     </div>
   );
 };
