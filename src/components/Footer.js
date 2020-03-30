dd81a40b76df3dcbd987c1eabd9be8fdb9211797
Leighton A Pitman <leightonpitman14@gmail.com>
2020-03-30 16:28:31 -0400

    header and button colors changed to coral theme

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,14 +8,28 @@ const Footer = () => {
       flexWrap="wrap"
       p={1}
       m={1}
-      style={{ height: "10vh", width: "100vw", backgroundColor: "black" }}
+      style={{
+        height: "10vh",
+        width: "100vw",
+        backgroundColor: "rgb(43, 43, 43)"
+      }}
       justifyContent="center"
     >
-      <Box style={{color: "white" }} p={1}>Home</Box>
-      <Box style={{color: "white" }} p={1}>Stay Connected</Box>
-      <Box style={{color: "white" }} p={1}>Return Policy</Box>
-      <Box style={{color: "white" }} p={1}>Privacy Policy</Box>
-      <Box style={{color: "white" }} p={1}>Terms and Conditions</Box>
+      <Box style={{ color: "white" }} p={1}>
+        Home
+      </Box>
+      <Box style={{ color: "white" }} p={1}>
+        Stay Connected
+      </Box>
+      <Box style={{ color: "white" }} p={1}>
+        Return Policy
+      </Box>
+      <Box style={{ color: "white" }} p={1}>
+        Privacy Policy
+      </Box>
+      <Box style={{ color: "white" }} p={1}>
+        Terms and Conditions
+      </Box>
     </Box>
   );
 };
